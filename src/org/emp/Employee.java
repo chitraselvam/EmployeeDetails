3997b0423c3011e240ffd49478245b74d987f2ae
chitraselvam <nchitra2015@gmail.com>
2021-09-04 14:35:28 +0530

    my new task

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,43 @@
+package org.emp;
+
+public class Employee {
+	//empId(),empName(),empDob(),empPhone(),empEmail(),empAddress()
+	
+	public void empId() {
+		// TODO Auto-generated method stub
+System.out.println("Employee Id is 40220");
+	}
+	public void empName() {
+		// TODO Auto-generated method stub
+System.out.println("Employee name is chitra");
+	}
+	public void empDob() {
+		// TODO Auto-generated method stub
+System.out.println("Employee DOB is 11/10/1988");
+	}
+	public void empPhone() {
+		// TODO Auto-generated method stub
+System.out.println("1234567890");
+	}
+	public void empEmail() {
+		// TODO Auto-generated method stub
+System.out.println("nchitra2015@gmail.com");
+	}
+	public void empAddress() {
+		// TODO Auto-generated method stub
+		System.out.println("chennai");
+
+	}
+	public static void main (String [] args)
+	{
+		Employee x = new Employee();
+		x.empAddress();
+		x.empDob();
+		x.empEmail();
+		x.empId();
+		x.empName();
+		x.empPhone();
+	}
+	}
+
+
